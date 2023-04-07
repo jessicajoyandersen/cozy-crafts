fe72f4ec222260c89c9ccad3a27e82cd2872bfe4
jessicajoyandersen <121067534+jessicajoyandersen@users.noreply.github.com>
2023-04-07 01:42:55 -0400

    index made, start of form

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+const element = document.getElementById("product-display");
+element.appendChild(para);
+
+para.style.display = "grid";*/
+
+
+
+// append account 
+//    registration/login
+//        and checkout
+
